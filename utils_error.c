5e086dcbec47f036c3b28b96c572c979418af553
Paulo Pinto <gomescasado@gmail.com>
2025-08-09 21:55:56 +0100

    Iniciando a tratativa de erros dos inputs

--- utils_error.c
+++ utils_error.c
@@ -6,31 +6,39 @@
 /*   By: paulo <paulo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/05 21:56:34 by paulo             #+#    #+#             */
-/*   Updated: 2025/08/09 19:32:07 by paulo            ###   ########.fr       */
+/*   Updated: 2025/08/09 19:47:25 by paulo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-// static int	ft_isdigit(char character)
-// {
-// 	return (character >= '0' && character <= '9');
-// }
+static int	ft_isdigit(char character)
+{
+	return (character >= '0' && character <= '9');
+}
 
-// static int	ft_issignal(char character)
-// {
-// 	return (character == '-' || character == '+');
-// }
+static int	ft_issignal(char character)
+{
+	return (character == '-' || character == '+');
+}
 
 int	verify_array_errors(char **split_list)
 {
 	int	i;
+	int	j;
 
-	printf("ERRORS\n");
 	i = 0;
 	while (split_list[i] != NULL)
 	{
-		printf("split_list[%d] = %s\n", i, split_list[i]);
+		j = 0;
+		while (split_list[i][j])
+		{
+			if (ft_issignal(split_list[i][j]) || ft_isdigit(split_list[i][j]))
+				printf("Eh digito ou signal\n");
+			else
+				printf("Eh qualquer outra coisa\n");
+			j++;
+		}
 		i++;
 	}
 
